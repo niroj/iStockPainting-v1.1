31840332491234753457296fb1e3a609d24543cc
Niroj Shrestha <niroj77@yahoo.com>
2012-01-04 16:53:20 +0545

    tags errors resolved

--- app/controllers/category_controller.rb
+++ app/controllers/category_controller.rb
@@ -1,4 +1,5 @@
 class CategoryController < ApplicationController
+  before_filter :tag_collector
   def show
     @category = Category.where(:id => params[:id]).last
     if @category.id == 1
@@ -6,5 +7,8 @@ class CategoryController < ApplicationController
     else
       @paintings = @category.paintings
     end
+  end
+  def tag_collector
+    @tags = Painting.tag_counts_on(:tags).limit(20)
   end
 end
--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,11 @@
 class HomeController < ApplicationController
+  before_filter :tag_collector
   def index
     if user_signed_in?
       redirect_to painting_path(8)
     end
+  end
+  def tag_collector
+    @tags = Painting.tag_counts_on(:tags).limit(20)
   end
 end
