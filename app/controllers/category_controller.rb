152df6900d5eb321a6e5abcb71b30b2a54ace3b3
Niroj Shrestha <niroj77@yahoo.com>
2012-01-06 10:30:06 +0545

    category default image

--- app/controllers/category_controller.rb
+++ app/controllers/category_controller.rb
@@ -1,5 +1,6 @@
 class CategoryController < ApplicationController
   before_filter :tag_collector
+  before_filter :authenticate_user!, :except => [:show, :index, :tag_collector]
   def index
     @categories = Category.all
   end
@@ -9,6 +10,18 @@ class CategoryController < ApplicationController
       @paintings = Painting.all
     else
       @paintings = @category.paintings
+    end
+  end
+  def new
+    @category = Category.new
+  end
+  def create
+    @category = Category.new(params[:category])
+    if @category.save
+      flash[:notice] = "Successfully uploaded category."
+      redirect_to @category, :action => 'index'
+    else
+      render :action => 'new'
     end
   end
   def tag_collector
